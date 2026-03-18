7f91eb394d10c0c58c06bbec62e959f9a04b1545
Elia <emb083@latech.edu>
2026-03-17 20:15:25 -0500

    fixed movement jittering (mostly)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,9 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
     private PlayerActions inputActions;
     public float movementSpeed = 6f;
-    private const float LEFT_LIMIT = -8.35f;
+    private const float LEFT_LIMIT = -8.4f;
     private const float RIGHT_LIMIT = 0f;
-    private const float Y_LIMIT = 4.46f;
+    private const float Y_LIMIT = 4.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,22 +26,34 @@ public class PlayerMovement : MonoBehaviour
         // controls
 
         if (input.MoveUp.IsPressed()){
-            this.transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+            Vector3 upVector = Vector3.up * movementSpeed * Time.deltaTime;
+            if ((upVector.magnitude + y_pos) < Y_LIMIT){
+               this.transform.Translate(upVector); 
+            }
         }
 
         if (input.MoveDown.IsPressed()){
-            this.transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+            Vector3 downVector = Vector3.down * movementSpeed * Time.deltaTime;
+            if ((downVector.magnitude + y_pos) > -Y_LIMIT){
+                this.transform.Translate(downVector);
+            }
         }
 
         if (input.MoveLeft.IsPressed()){
-            this.transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            Vector3 leftVector = Vector3.left * movementSpeed * Time.deltaTime;
+            if ((leftVector.magnitude + x_pos) > LEFT_LIMIT){
+                this.transform.Translate(leftVector);  
+            }
         }
 
         if (input.MoveRight.IsPressed()){
-            this.transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            Vector3 rightVector = Vector3.right * movementSpeed * Time.deltaTime;
+            if ((rightVector.magnitude + x_pos) < RIGHT_LIMIT){
+                this.transform.Translate(rightVector);  
+            }
         }
 
-        // bounds
+        // bounds (double-checking)
 
         if (y_pos > Y_LIMIT){
             this.transform.position = new Vector3(x_pos, Y_LIMIT);

42a9fc6864b0ce84d8ef387ff18bdc7a47c9bb71
Elia <emb083@latech.edu>
2026-03-21 13:57:03 -0500

    Added screen bounds, destroy bullets when offscreen

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,8 +5,13 @@ public class Bullet : MonoBehaviour
     // set in inspector
     public float bulletSpeed = 90f;
 
-    void Update()
-    {
+    void Update() {
         this.transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+    if (collision.CompareTag("Bounds")) {
+        Destroy(gameObject);
+    }
+  }
 }
